17c991772bbac71f24a82b3065ce4d1d12c09d67
alwindeleon <bludeleon@gmail.com>
2016-12-12 14:24:14 +0800

    add on/off button

--- extension/js/app.js
+++ extension/js/app.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
+	chrome.storage.local.get('status', function(status_obj){
+		if(status_obj.status){
+			$('#click').text('turn off');
+		}
+		else if( !status_obj.status ){
+			$('#click').text('turn on')
+		}
+	});
+
 	$('#click').click(function(){
-		alert("asdasd");
+		chrome.storage.local.get('status', function(status_obj){
+			if ( status_obj.status ){
+				// hide all spoilers
+				chrome.storage.local.set({'status': false}, function(){
+					console.log('status is now false');
+					$('#click').text('turn on');
+				});
+
+			}else if( !status_obj.status ){
+				// show all spoilers
+				chrome.storage.local.set({'status': true}, function(){
+					console.log('status is now true');
+					$('#click').text('turn off');
+				});
+			}
+		});
 	});
 });
--- extension/js/contentscript.js
+++ extension/js/contentscript.js
@@ -1,13 +1,43 @@
 $(document).ready(function(){
-	$('div._1dwg._1w_m').hide();
+
+	chrome.storage.local.get('status', function(status_obj){
+		if(status_obj.status == null){
+			chrome.storage.local.set({'status': true});
+			$('div._1dwg._1w_m').hide();
+		}else if ( status_obj.status ){
+			// hide all spoilers
+			$('div._1dwg._1w_m').hide();
+		}else if( !status_obj.status ){
+			// show all spoilers
+			$('div._1dwg._1w_m').show();
+		}
+	});
+
 	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 	var observer = new MutationObserver(function(mutations, observer) {
-	    console.log(mutations, observer);
-	    $('div._1dwg._1w_m').hide();
+		chrome.storage.local.get('status',function(status_obj){
+			if(status_obj.status){
+				$('div._1dwg._1w_m').hide();
+			} else if( !status_obj.status){
+				$('div._1dwg._1w_m').show();
+			}
+		});
+	    
 	});
 	observer.observe(document, {
 	  subtree: true,
 	  attributes: true
 	});
+
+	chrome.storage.onChanged.addListener(function(changes, namespace) {
+      var status = changes.status;
+      if(status){
+      	$('div._1dwg._1w_m').hide();
+      }else if(!status){
+      	$('div._1dwg._1w_m').show();
+      }
+      
+
+    });
 });
