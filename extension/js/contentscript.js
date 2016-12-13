0e76c5e6621dad4b3a9bcdd20b6ae7a711b62a33
alwindeleon <bludeleon@gmail.com>
2016-12-14 00:54:22 +0800

    integrated

--- extension/js/contentscript.js
+++ extension/js/contentscript.js
@@ -1,4 +1,17 @@
+var d, doc;
+
 $(document).ready(function(){
+
+	d = {'true':{'total':0}, 'false':{'total':0}};
+	var sizeVocab = 0;
+	doc = {'true':0, 'false':0};
+
+	$.getJSON('https://extension-server.herokuapp.com/dictionary',function(res){
+		d = res;
+	})
+	$.getJSON('https://extension-server.herokuapp.com/doc',function(res){
+		doc = res;
+	})
 
 	chrome.storage.local.get('status', function(status_obj){
 		if(status_obj.status == null){
@@ -40,17 +53,43 @@ $(document).ready(function(){
       
 
     });
+
 });
 
-function isSpoiler(text){
-	console.log(text);
-	return true;	
+
+
+function isSpoiler(inputText){
+	var guess = false;
+	console.log(inputText.split(" "));
+	inputText = String(inputText);
+	var trueProb = 1 * doc.true
+	var falseProb = 1 * doc.false
+	var inputText = String(inputText.replace(/[^A-Za-z0-9 ]/g, ''));
+	console.log(inputText)
+	var words = inputText.split(" ").map(function(x){return x.toLowerCase()}).filter(function(x,i,a){ return a.indexOf(x)==i});
+	console.log(words);
+	for(var i = 0; i < words.length ;i++){
+		if( words[i] != "" && d['true'].hasOwnProperty(words[i]) && d['false'].hasOwnProperty(words[i]) ){
+			trueProb = trueProb * d['true'][words[i]]
+			falseProb = falseProb * d['false'][words[i]]
+		}
+	}
+	console.log( trueProb, falseProb)
+	if (trueProb > falseProb){
+		guess = true;
+	}
+	else{
+		guess = false;
+	}
+		
+	return guess
 }
 
 function blockSpoilers ( container ){
 	return container.children('div').children('.userContent').children('p').each(function(){
-		if( isSpoiler( $(this).text() ) ){
+		if( isSpoiler( $(this).text()) ){
 			$(this).parent().parent().parent().hide();
 		}
 	});
 }
+
